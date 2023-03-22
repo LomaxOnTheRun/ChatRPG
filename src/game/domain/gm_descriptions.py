523c6f2a78ec28adaa1a2a50cd64461f3c6bb50b
Ben Lomax <lomax.on.the.run@gmail.com>
2023-03-22 21:47:22 +0000

    Use the last 3 messages to create the GM response

--- src/game/domain/gm_descriptions.py
+++ src/game/domain/gm_descriptions.py
@@ -1,5 +1,6 @@
 import openai
 
+from . import utils
 from .. import models
 
 
@@ -30,7 +31,18 @@ def get_one_player_game_intro(character: models.Character) -> str:
 def get_one_player_game_next_description(
     character: models.Character, game: models.OnePlayerGame
 ) -> str:
-    previous_messages = _get_previous_messages(game)
+    """
+    Get a description from the GM of a one-player game, responding the latest prompt
+    from the player.
+    """
+
+    previous_messages = _get_previous_messages(game, max_num_turns=3)
+
+    # Add a prompt to the final message to try and shape the response to be "better"
+    final_prompt = (
+        "Describe what happens next in the story. Only write a short paragraph."
+    )
+    previous_messages[-1]["content"] = previous_messages[-1]["content"] + final_prompt
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -64,3 +76,29 @@ def _get_previous_messages(game: models.OnePlayerGame) -> list[dict[str, str]]:
             "story. Only write a short paragraph.",
         }
     ]
+
+
+def _get_previous_messages(
+    game: models.OnePlayerGame, max_num_turns: int
+) -> list[dict[str, str]]:
+    """
+    Return the previous messages in the "conversation" between the GM and the player.
+    """
+
+    lastest_game_turns = utils.get_latest_one_player_game_turns(game, max_num_turns)
+
+    previous_messages = []
+    for turn in lastest_game_turns:
+        role = _get_role(turn)
+        previous_messages.append({"role": role, "content": turn.description})
+
+    return previous_messages
+
+
+def _get_role(turn: models.OnePlayerGameTurn) -> str:
+    """
+    The GM is currently the AI (or "assistant"), while the player is the external
+    "user", who is asking questions of the GM / "assistant".
+    """
+
+    return "assistant" if turn.character is None else "user"
