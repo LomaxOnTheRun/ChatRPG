b7eff633b065cd324c4f3c7cf3e8b3a5e7c8f39d
Ben Lomax <lomax.on.the.run@gmail.com>
2023-03-22 20:31:40 +0000

    Create admin views for all models

--- src/game/admin.py
+++ src/game/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ("name", "race_name", "class_name")
+
+
+@admin.register(models.OnePlayerGame)
+class OnePlayerGameAdmin(admin.ModelAdmin):
+    list_display = ("id", "character")
+
+
+@admin.register(models.OnePlayerGameTurn)
+class OnePlayerGameAdmin(admin.ModelAdmin):
+    list_display = ("game", "character", "description")
