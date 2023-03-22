8a50de58c1e723b6cfc270e61ac5deb59005a8c5
Ben Lomax <lomax.on.the.run@gmail.com>
2023-03-22 17:21:03 +0000

    Show links to one player game pages for existing characters

--- src/game/views.py
+++ src/game/views.py
@@ -7,6 +7,13 @@ from . import forms, models
 class Home(generic.TemplateView):
     template_name = "home.html"
 
+    def get_context_data(self, **kwargs: object) -> dict[str, object]:
+        context = super().get_context_data(**kwargs)
+
+        context["characters"] = models.Character.objects.all()
+
+        return context
+
 
 class CharacterCreation(generic.FormView):
     template_name = "character-creation.html"
