b3f509a461ae81cfc76449d356b73a6e33596419
Ben Lomax <lomax.on.the.run@gmail.com>
2023-03-22 15:26:34 +0000

    Add `Character` model

--- src/game/models.py
+++ src/game/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    name = models.CharField(max_length=200)
+    race_name = models.CharField(max_length=100)
+    class_name = models.CharField(max_length=100)
+    visual_description = models.CharField(max_length=1000)
+    personality = models.CharField(max_length=1000)
+    backstory = models.CharField(max_length=1000)
+
+    # Audit fields
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
