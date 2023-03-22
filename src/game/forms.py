b1c55ed41c8e4eb17e4702547b67864f605b602d
Ben Lomax <lomax.on.the.run@gmail.com>
2023-03-22 16:51:01 +0000

    Move form into new `forms.py` file

---
+++ src/game/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+from . import domain
+
+
+class CharacterCreationForm(forms.Form):
+    name = forms.CharField(label="Character name", max_length=200)
+    race_name = forms.CharField(label="Race", max_length=100)
+    class_name = forms.CharField(label="Class", max_length=100)
+    visual_description = forms.CharField(
+        label="Visual description", max_length=1000, widget=forms.Textarea
+    )
+    personality = forms.CharField(
+        label="Personality", max_length=1000, widget=forms.Textarea
+    )
+    backstory = forms.CharField(
+        label="Backstory", max_length=2000, widget=forms.Textarea
+    )
+
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        super().__init__(*args, **kwargs)
+
+        # Get character description
+        character_description = domain.get_openai_character_description()
+        self.fields["name"].initial = character_description.name
+        self.fields["race_name"].initial = character_description.race_name
+        self.fields["class_name"].initial = character_description.class_name
+        self.fields[
+            "visual_description"
+        ].initial = character_description.visual_description
+        self.fields["personality"].initial = character_description.personality
+        self.fields["backstory"].initial = character_description.backstory
--- src/game/views.py
+++ src/game/views.py
@@ -1,44 +1,15 @@
 from django import forms, http
 from django.views import generic
 
-from . import domain, models
-
-
-class CharacterCreationForm(forms.Form):
-    name = forms.CharField(label="Character name", max_length=200)
-    race_name = forms.CharField(label="Race", max_length=100)
-    class_name = forms.CharField(label="Class", max_length=100)
-    visual_description = forms.CharField(
-        label="Visual description", max_length=1000, widget=forms.Textarea
-    )
-    personality = forms.CharField(
-        label="Personality", max_length=1000, widget=forms.Textarea
-    )
-    backstory = forms.CharField(
-        label="Backstory", max_length=2000, widget=forms.Textarea
-    )
-
-    def __init__(self, *args: object, **kwargs: object) -> None:
-        super().__init__(*args, **kwargs)
-
-        # Get character description
-        character_description = domain.get_openai_character_description()
-        self.fields["name"].initial = character_description.name
-        self.fields["race_name"].initial = character_description.race_name
-        self.fields["class_name"].initial = character_description.class_name
-        self.fields[
-            "visual_description"
-        ].initial = character_description.visual_description
-        self.fields["personality"].initial = character_description.personality
-        self.fields["backstory"].initial = character_description.backstory
+from . import forms, models
 
 
 class CharacterCreation(generic.FormView):
     template_name = "character-creation.html"
-    form_class = CharacterCreationForm
+    form_class = forms.CharacterCreationForm
     success_url = "/"
 
-    def form_valid(self, form: forms.Form) -> http.HttpResponse:
+    def form_valid(self, form: forms.CharacterCreationForm) -> http.HttpResponse:
         if form.is_valid():
             models.Character.objects.create(
                 name=form.cleaned_data["name"],
